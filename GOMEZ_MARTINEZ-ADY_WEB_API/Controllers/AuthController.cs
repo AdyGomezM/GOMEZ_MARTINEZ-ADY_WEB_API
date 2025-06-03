855f38bed930aad47065145e6d85295bbc22d1a9
adygg <adygg@ADYG>
2025-06-03 14:20:41 -0500

    Documentacion

---
+++ GOMEZ_MARTINEZ-ADY_WEB_API/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Domain.DTO;
+using GOMEZ_MARTINEZ_ADY_WEB_API.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GOMEZ_MARTINEZ_ADY_WEB_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IAuthServices _authService;
+
+        public AuthController(IAuthServices authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            var token = await _authService.Login(request);
+            return Ok(new { token });
+        }
+    }
+}
---
+++ GOMEZ_MARTINEZ-ADY_WEB_API/Services/IServices/IAuthServices.cs
@@ -0,0 +1,9 @@
+﻿using Domain.DTO;
+
+namespace GOMEZ_MARTINEZ_ADY_WEB_API.Services.IServices
+{
+    public interface IAuthServices
+    {
+        Task<string> Login(LoginRequest request);
+    }
+}
